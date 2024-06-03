0d5d525cbcf579dcbdf9a98794fa77b26ceac459
Cairo <cairosamuelferreiracontato@gmail.com>
2024-06-03 00:59:18 -0300

    COMMIT FINAL OU QUASE

--- portaldorpgista/script.js
+++ portaldorpgista/script.js
@@ -6,7 +6,7 @@ function resetBackground() {
     document.body.style.backgroundImage = "url('imagens/dd.jpg')";
 }
 
-// Função para rolar suavemente até uma seção
+
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -17,26 +17,26 @@ function scrollToSection(sectionId) {
     }
 }
 
-// Seleciona todos os links do menu
+
 const links = document.querySelectorAll('nav ul li a');
 
-// Adiciona um evento de clique a cada link
+
 links.forEach(link => {
     link.addEventListener('click', (event) => {
-        event.preventDefault(); // Previne o comportamento padrão do link
-        const sectionId = link.getAttribute('href').substring(1); // Obtém o ID da seção
-        scrollToSection(sectionId); // Chama a função para rolar até a seção
+        event.preventDefault();
+        const sectionId = link.getAttribute('href').substring(1);
+        scrollToSection(sectionId);
     });
 });
 
-// Seleciona o link "Sobre nós" do menu
+
 const linkSobreNos = document.querySelector('nav ul li a[href="#sobrenos"]');
 
-// Adiciona um evento de clique ao link
+
 linkSobreNos.addEventListener('click', (event) => {
-    event.preventDefault(); // Previne o comportamento padrão do link
-    const sectionId = linkSobreNos.getAttribute('href').substring(1); // Obtém o ID da seção
-    scrollToSection(sectionId); // Chama a função para rolar até a seção
+    event.preventDefault();
+    const sectionId = linkSobreNos.getAttribute('href').substring(1);
+    scrollToSection(sectionId);
 });
 
 
